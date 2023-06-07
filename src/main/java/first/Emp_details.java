a3e51eda3f0672d213a43cada45f2235ee1792f5
harishrao7 <ailneniharishrao7@gmail.com>
2023-06-06 21:16:43 -0400

    modified

--- src/main/java/first/Emp_details.java
+++ src/main/java/first/Emp_details.java
@@ -2,13 +2,28 @@ package first;
 
 public class Emp_details {
 
+	String empName="Hari";
+	double empSalary=100;
+	String empDept="HR";
+	int id=1343;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String empName="Hari";
-		double empSalary=100;
-		String empDept="HR";
-		int id=1343;
-System.out.println(empName +empSalary+ empDept+ id);
+		Emp_details e=new Emp_details();
+		System.out.println(e.calculateSalary(254.7));
+System.out.println(e.dept());
+e.empName("mounika");
 	}
+	
+	public double calculateSalary(double sal) {
+		return sal*12;
+	}
+public String dept() {
+	return empDept;
+}
+
+public void empName(String empName ) {
+	System.out.println(empName);
+}
+
 
 }
--- src/main/java/first/Person.java
+++ src/main/java/first/Person.java
@@ -1,14 +1,22 @@
 package first;
 
 public class Person {
-
+	int pid=1;
+	String address="345 Luxury,NC";
+	int age=12;
+	String phone="+14764664646";
 	public static void main(String[] args) {
-
-		int pid=1;
-		String address="345 Luxury,NC";
-		int age=12;
-		String phone="+14764664646";
-		System.out.println(pid+address+age+phone);
+   Person p=new Person();
+   p.introduce();
+  System.out.println(p.age(25)); 
+		
+		
+	}
+	void introduce() {
+		System.out.println("Hello");
+	}
+	public int age(int age) {
+		return age;
 	}
 
 }
